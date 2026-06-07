ef784be7515378f386af4c5704a4cb77c89212d6
Alex Duta <AlexGeorge22.alexandruduta@yahoo.com>
2026-06-07 23:20:47 +0300

    Add a tap-to-reveal eye on the password field

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget{
+class MyTextField extends StatefulWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obscureText;
@@ -13,25 +13,56 @@ class MyTextField extends StatelessWidget{
   });
 
   @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  // Pornește ascuns dacă e câmp de parolă; butonul „ochi" comută starea asta.
+  late bool _obscured = widget.obscureText;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-        padding:const EdgeInsets.symmetric(horizontal: 25.0),
-        child: TextField(
-            controller: controller,
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-              ) ,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey.shade400),
-              ),
-              fillColor: Colors.grey.shade200,
-              filled: true,
-              hintText: hintText,
-              hintStyle: TextStyle(color: Colors.grey[500]),
-            )
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: _obscured,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: widget.hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+          // Butonul în formă de ochi apare doar la câmpurile de parolă și comută
+          // între afișarea și ascunderea textului introdus. Iconița face o
+          // tranziție lină (fade + scale) la apăsare, ca în aplicațiile uzuale.
+          suffixIcon: widget.obscureText
+              ? IconButton(
+                  tooltip: _obscured ? 'Arată parola' : 'Ascunde parola',
+                  onPressed: () => setState(() => _obscured = !_obscured),
+                  icon: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 250),
+                    switchInCurve: Curves.easeOut,
+                    switchOutCurve: Curves.easeIn,
+                    transitionBuilder: (child, animation) => ScaleTransition(
+                      scale: animation,
+                      child: FadeTransition(opacity: animation, child: child),
+                    ),
+                    child: Icon(
+                      _obscured ? Icons.visibility_off : Icons.visibility,
+                      key: ValueKey<bool>(_obscured),
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                )
+              : null,
         ),
+      ),
     );
   }
 }
